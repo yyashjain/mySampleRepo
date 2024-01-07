10b2b3fcbf40077435d3dd0493c0f5489d8adc30
Sneha-hannovit <sneha.jain@hannovit.com>
2024-01-07 19:09:10 +0530

    added angular project

---
+++ Sample-Angular-Repo/src/app/first-component/first-component.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.scss']
+})
+export class FirstComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  clickMe(){
+    
+  }
+
+}
